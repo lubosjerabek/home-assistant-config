2b772a74bedb78fd8b19faf47f62fa0bb8b673e3
Lubos Jerabek <lubos.jerabek@gmail.com>
2020-12-27 12:52:50 +0000

    SSL conversion script

---
+++ convert_ssl.sh
@@ -0,0 +1,7 @@
+rm certs/*
+
+openssl x509 -in ssl_all/SSLcertificate.crt -out certs/SSLcertificate.pem
+openssl x509 -in ssl_all/SSLIntermediateCertificate.crt -out certs/SSLIntermediateCertificate.pem
+
+cat certs/SSLcertificate.pem certs/SSLIntermediateCertificate.pem > certs/fullchain.pem
+cp ssl_all/SSLprivatekey.key certs/SSLprivatekey.key
