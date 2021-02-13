cc3a539d231bc1b0b266fd0893c24602eafe346e
Lubos Jerabek <lubos.jerabek@gmail.com>
2021-02-13 16:57:26 +0000

    Adding in whitespace character

--- convert_ssl.sh
+++ convert_ssl.sh
@@ -6,4 +6,4 @@ openssl x509 -in ssl_all/SSLIntermediateCertificate.crt -out certs/SSLIntermedia
 cat certs/SSLcertificate.pem certs/SSLIntermediateCertificate.pem > certs/fullchain.pem
 cp ssl_all/SSLprivatekey.key certs/SSLprivatekey.key
 
-# Jenkins test again
+# Jenkins test again 
